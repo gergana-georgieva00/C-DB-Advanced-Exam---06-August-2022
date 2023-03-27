0ac0170874bc91cced839fc5034a824e06648a54
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-27 15:58:03 +0300

    Ex 1

---
+++ Footballers/Data/Models/Footballer.cs
@@ -0,0 +1,19 @@
+﻿using Footballers.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public class Footballer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public DateTime ContractEndDate { get; set; }
+        public PositionType Position { get; set; }
+        public BestSkillType BestSkill { get; set; }
+    }
+}
