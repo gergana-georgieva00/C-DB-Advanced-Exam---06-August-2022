e71b35a89eb0cd1491501d53236e08b96f9f6104
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-27 18:43:30 +0300

    Ex 2

--- Footballers/DataProcessor/Deserializer.cs
+++ Footballers/DataProcessor/Deserializer.cs
@@ -1,7 +1,12 @@
 ﻿namespace Footballers.DataProcessor
 {
     using Footballers.Data;
+    using Footballers.Data.Models;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ImportDto;
     using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Deserializer
     {
@@ -15,7 +20,65 @@
 
         public static string ImportCoaches(FootballersContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            var coachDtos = Deserialize<ImportXmlCoach[]>(xmlString, "Coaches");
+            var sb = new StringBuilder();
+            var coaches = new List<Coach>();
+
+            foreach (var dto in coachDtos)
+            {
+                if (!IsValid(dto) || string.IsNullOrEmpty(dto.Nationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var coach = new Coach()
+                {
+                    Name = dto.Name,
+                    Nationality = dto.Nationality
+                };
+
+                var footballers = new List<Footballer>();
+                foreach (var footballerDto in dto.Footballers)
+                {
+                    if (!IsValid(footballerDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    var footballer = new Footballer()
+                    {
+                        Name = footballerDto.Name,
+                        ContractStartDate = footballerDto.ContractStartDate,
+                        ContractEndDate = footballerDto.ContractEndDate,
+                        BestSkillType = (BestSkillType)footballerDto.BestSkillType,
+                        PositionType = (PositionType)footballerDto.PositionType
+                    };
+
+                    footballers.Add(footballer);
+                }
+
+                coach.Footballers = footballers;
+                coaches.Add(coach);
+                sb.AppendLine(string.Format(SuccessfullyImportedCoach, coach.Name, coach.Footballers.Count()));
+            }
+
+            context.AddRange(coaches);
+            context.SaveChanges();
+
+            return sb.ToString();
+        }
+
+        private static T Deserialize<T>(string inputXml, string rootName)
+        {
+            var root = new XmlRootAttribute(rootName);
+            var serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
         }
 
         public static string ImportTeams(FootballersContext context, string jsonString)
--- Footballers/DataProcessor/ImportDto/ImportXmlCoach.cs
+++ Footballers/DataProcessor/ImportDto/ImportXmlCoach.cs
@@ -18,6 +18,27 @@ namespace Footballers.DataProcessor.ImportDto
         [XmlElement("Nationality")]
         [Required]
         public string Nationality { get; set; }
-        public List<Footballer> Footballers { get; set; }
+        [XmlArray("Footballers")]
+        public FootballerDto[] Footballers { get; set; }
+    }
+
+    [XmlType("Footballer")]
+    public class FootballerDto
+    {
+        [XmlElement("Name")]
+        [Required, MinLength(2), MaxLength(40)]
+        public string Name { get; set; }
+        [XmlElement("ContractStartDate")]
+        [Required]
+        public DateTime ContractStartDate { get; set; }
+        [XmlElement("ContractEndDate")]
+        [Required]
+        public DateTime ContractEndDate { get; set; }
+        [XmlElement("BestSkillType")]
+        [Required, Range(0, 4)]
+        public int BestSkillType { get; set; }
+        [XmlElement("PositionType")]
+        [Required, Range(0, 3)]
+        public int PositionType { get; set; }
     }
 }
