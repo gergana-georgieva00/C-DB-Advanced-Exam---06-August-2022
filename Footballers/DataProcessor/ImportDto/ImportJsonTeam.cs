e760c2fea2121b11cfa7fd7d0a2ea101e0ea51f4
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-28 11:48:12 +0300

    Ex 2

---
+++ Footballers/DataProcessor/ImportDto/ImportJsonTeam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class ImportJsonTeam
+    {
+        [Required, MinLength(3), MaxLength(40), RegularExpression(@"[a-zA-Z0-9\.\-\s]+")]
+        public string Name { get; set; }
+        [Required, MinLength(2), MaxLength(40)]
+        public string Nationality { get; set; }
+        [Required]
+        public int Trophies { get; set; }
+        public int[] Footballers { get; set; }
+    }
+}
