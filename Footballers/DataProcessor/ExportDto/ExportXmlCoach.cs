13d17d0a2dc4158495c6989aa631ea853658f628
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-28 17:06:05 +0300

    Ex 3

---
+++ Footballers/DataProcessor/ExportDto/ExportXmlCoach.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Coach")]
+    public class ExportXmlCoach
+    {
+        [XmlAttribute("FootballersCount")]
+        public int FootballersCount { get; set; }
+        [XmlElement("CoachName")]
+        public string CoachName { get; set; }
+        [XmlArray("Footballers")]
+        public ExportFootballerDto[] Footballers { get; set; }
+    }
+
+    [XmlType("Footballer")]
+    public class ExportFootballerDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        [XmlElement("Position")]
+        public string Position { get; set; }
+    }
+}
