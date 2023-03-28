03d290014ac0eb65c5068f1e97e047b267d8caf1
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-28 17:55:44 +0300

    Exercise 3

--- Footballers/DataProcessor/Serializer.cs
+++ Footballers/DataProcessor/Serializer.cs
@@ -54,28 +54,28 @@
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
-            var startDate = DateTime.TryParseExact(date.ToString(), "dd/MM/yyyy",
-                        CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime validStartDate);
-
-            var teams = context.Teams
+            var teams = context
+                .Teams
                 .Where(t => t.TeamsFootballers
-                .Any(tf => tf.Footballer.ContractStartDate >= validStartDate))
+                .Any(tf => tf.Footballer.ContractStartDate >= date))
                 .ToArray()
                 .Select(t => new
                 {
                     Name = t.Name,
                     Footballers = t.TeamsFootballers
-                    .Where(tf => tf.Footballer.ContractStartDate >= validStartDate)
+                    .Where(tf => tf.Footballer.ContractStartDate >= date)
+                    .OrderByDescending(tf => tf.Footballer.ContractEndDate)
+                    .ThenBy(tf => tf.Footballer.Name)
+                    .ToArray()
                     .Select(tf => new
                     {
-                        Name = tf.Footballer.Name,
-                        ContractStartDate = tf.Footballer.ContractStartDate,
-                        ContractEndDate = tf.Footballer.ContractEndDate,
-                        BestSkillType = tf.Footballer.BestSkillType.ToString("d"),
-                        PositionType = tf.Footballer.PositionType.ToString("d")
+                        FootballerName = tf.Footballer.Name,
+                        ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                        ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                        BestSkillType = tf.Footballer.BestSkillType.ToString(),
+                        PositionType = tf.Footballer.PositionType.ToString()
                     })
-                    .OrderByDescending(f => f.ContractEndDate)
-                    .ThenBy(f => f.Name)
+                    .ToArray()
                 })
                 .OrderByDescending(t => t.Footballers.Count())
                 .ThenBy(t => t.Name)
